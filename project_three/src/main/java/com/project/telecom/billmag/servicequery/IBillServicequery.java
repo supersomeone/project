8bd498612d603c9ed1a86e5333e7ff46c96169aa
sr0701 <331258430@qq.com>
2018-06-14 16:22:22 +0800

    更改

--- project_three/src/main/java/com/project/telecom/billmag/servicequery/IBillServicequery.java
+++ project_three/src/main/java/com/project/telecom/billmag/servicequery/IBillServicequery.java
@@ -2,12 +2,12 @@ package com.project.telecom.billmag.servicequery;
 
 import java.util.Map;
 
-import com.project.telecom.beans.PagerBean;
+import com.project.telecom.beans.Pager;
 
 /**
  * @author ASUS
  * @version 1.0
- * @created 14-6ÔÂ-2018 9:25:56
+ * @created 14-6ï¿½ï¿½-2018 9:25:56
  */
 public interface IBillServicequery {
 
