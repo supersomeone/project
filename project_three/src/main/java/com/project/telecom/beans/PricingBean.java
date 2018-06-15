8cf42ff0b205fc6163f69f278409642fcfcfce39
哈尼 <@DESKTOP-KKNROTV>
2018-06-15 15:39:49 +0800

    资费 bean 的定义注解

--- project_three/src/main/java/com/project/telecom/beans/PricingBean.java
+++ project_three/src/main/java/com/project/telecom/beans/PricingBean.java
@@ -1,55 +1,177 @@
 package com.project.telecom.beans;
 
+import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
 /**
- * ×Ê·ÑĞÅÏ¢
+ * ï¿½Ê·ï¿½ï¿½ï¿½Ï¢
  * @author ASUS
  * @version 1.0
- * @created 14-6ÔÂ-2018 9:25:59
+ * @created 14-6ï¿½ï¿½-2018 9:25:59
  */
-public class PricingBean {
+@Entity
+@Table(name="t_pricing")
+public class PricingBean implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5489871993783790245L;
+	@Id
+	@Column
+	@GenericGenerator(name="hibernate.id",strategy="identity")
+	@GeneratedValue(generator="hibernate.id")
+	private long id;//id
+	
+	@Column(name="pricing_name",length=20)
+	private String pricingName;//èµ„è´¹åç§°
+	
+	@Column(name="pricing_type")
+	private int pricingType;//èµ„è´¹ç±»å‹
+	
+	@Column
+	private int basictime;//åŸºæœ¬æ—¶é•¿
+	
+	@Column
+	private double charge;//åŸºæœ¬è´¹ç”¨
+	
+	@Column
+	private double unitcost;//å•ä½è´¹ç”¨
+	
+	@Column
+	private Date opentime;//å¼€é€šæ—¶é—´
+	
+	@Column(name="pricing_fee",length=20)
+	private String pricingFee;//èµ„è´¹è¯´æ˜
 
-	/**
-	 * »ù±¾Ê±³¤
-	 */
-	private int basictime;
-	/**
-	 * »ù±¾·ÑÓÃ
-	 */
-	private double charge;
-	private long id;
-	/**
-	 * ¿ªÍ¨Ê±¼ä
-	 */
-	private Date opentime;
-	/**
-	 * ×Ê·ÑËµÃ÷
-	 */
-	private String pricingFee;
-	/**
-	 * ×Ê·ÑÃû³Æ
-	 */
-	private String pricingName;
-	/**
-	 * ×´Ì¬
-	 */
-	private int pricingState;
-	/**
-	 * ×Ê·ÑÀàĞÍ
-	 */
-	private int pricingType;
-	/**
-	 * µ¥Î»·ÑÓÃ
-	 */
-	private double unitcost;
+	@Column(name="pricing_state")
+	private int pricingState;//å¼€æ²¡å¼€é€šçš„çŠ¶æ€
+	
 
 	public PricingBean(){
 
 	}
 
-	public void finalize() throws Throwable {
 
+	public PricingBean(long id, String pricingName, int pricingType, int basictime, double charge, double unitcost,
+			Date opentime, String pricingFee, int pricingState) {
+		super();
+		this.id = id;
+		this.pricingName = pricingName;
+		this.pricingType = pricingType;
+		this.basictime = basictime;
+		this.charge = charge;
+		this.unitcost = unitcost;
+		this.opentime = opentime;
+		this.pricingFee = pricingFee;
+		this.pricingState = pricingState;
 	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getPricingName() {
+		return pricingName;
+	}
+
+
+	public void setPricingName(String pricingName) {
+		this.pricingName = pricingName;
+	}
+
+
+	public int getPricingType() {
+		return pricingType;
+	}
+
+
+	public void setPricingType(int pricingType) {
+		this.pricingType = pricingType;
+	}
+
+
+	public int getBasictime() {
+		return basictime;
+	}
+
+
+	public void setBasictime(int basictime) {
+		this.basictime = basictime;
+	}
+
+
+	public double getCharge() {
+		return charge;
+	}
+
+
+	public void setCharge(double charge) {
+		this.charge = charge;
+	}
+
+
+	public double getUnitcost() {
+		return unitcost;
+	}
+
+
+	public void setUnitcost(double unitcost) {
+		this.unitcost = unitcost;
+	}
+
+
+	public Date getOpentime() {
+		return opentime;
+	}
+
+
+	public void setOpentime(Date opentime) {
+		this.opentime = opentime;
+	}
+
+
+	public String getPricingFee() {
+		return pricingFee;
+	}
+
+
+	public void setPricingFee(String pricingFee) {
+		this.pricingFee = pricingFee;
+	}
+
+
+	public int getPricingState() {
+		return pricingState;
+	}
+
+
+	public void setPricingState(int pricingState) {
+		this.pricingState = pricingState;
+	}
+
+
+	@Override
+	public String toString() {
+		return "PricingBean [id=" + id + ", pricingName=" + pricingName + ", pricingType=" + pricingType
+				+ ", basictime=" + basictime + ", charge=" + charge + ", unitcost=" + unitcost + ", opentime="
+				+ opentime + ", pricingFee=" + pricingFee + ", pricingState=" + pricingState + "]";
+	}
+
+	
 
 }
